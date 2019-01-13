4a25183f12ff26d4a9d49430e52a1c5aff927e70
paulcysu <su.paul.cy@gmail.com>
2019-01-12 20:56:21 -0800

    executed strategy and passport

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 // import express from 'express' (needs babel setup)
 const express = require('express')
 
+// import passport and strategy
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 // express() - generates a new app
 const app = express();
 
-// create a route handler in app to send response of "Hi, there!"
-app.get('/', (req, res) => {
-    res.send('Hi, there!')
-});
+passport.use(new GoogleStrategy());
 
 // listen to see if there a port to be used (used this for Heroku, because it gives PORT in last second)
 const PORT = process.env.PORT || 5000
